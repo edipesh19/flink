d5a0b902d6016afd3ddcca355dc2be392ac7b3b4
Dipesh Dutta <dipeshkumar.dutta@gmail.com>
2019-09-19 22:40:08 +0530

    Implement word count Stream

---
+++ wordcountstream/src/main/java/com/learn/flink/wordcount/stream/WordcountStream.java
@@ -0,0 +1,44 @@
+package com.learn.flink.wordcount.stream;
+
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class WordcountStream {
+
+// nc -l 8998
+    public static void main(String[] args) throws Exception
+    {
+        // set up the stream execution environment
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        // Checking input parameters
+        final ParameterTool params = ParameterTool.fromArgs(args);
+
+        // make parameters available in the web interface
+        env.getConfig().setGlobalJobParameters(params);
+
+        DataStream<String> text = env.socketTextStream("localhost", 8998);
+
+        DataStream<Tuple2<String, Integer>> counts =  text.filter((FilterFunction<String>) value -> value.startsWith("D"))
+
+            .map(new Tokenizer())         // split up the lines in pairs (2-tuples) containing: tuple2 {(name,1)...}
+            .keyBy(0).sum(1);            // group by the tuple field "0" and sum up tuple field "1"
+
+        counts.print();
+
+        // execute programßßß
+        env.execute("Streaming WordCount");
+    }
+
+    public static final class Tokenizer  implements MapFunction<String, Tuple2<String, Integer>>
+    {
+        public Tuple2<String, Integer> map(String value)
+        {
+            return new Tuple2(value, Integer.valueOf(1));
+        }
+    }
+}
