f59044149812772ef7add229d498b9a7cc6f25fb
Dipesh Dutta <dipeshkumar.dutta@gmail.com>
2019-11-20 13:38:22 +0530

    Implement jsonPath demo

--- jsonpath/src/main/java/com/demo/jasonpath/JsonPathDemo.java
+++ jsonpath/src/main/java/com/demo/jasonpath/JsonPathDemo.java
@@ -1,9 +1,42 @@
 package com.demo.jasonpath;
 
-import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.Configuration;
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.Option;
+import net.minidev.json.JSONObject;
+
+import java.util.List;
+
+import static com.jayway.jsonpath.JsonPath.using;
 
 public class JsonPathDemo {
-    public static String getData(String jsonData, String jsonPath) {
+
+    private static Configuration configuration = Configuration.builder().
+        options(Option.DEFAULT_PATH_LEAF_TO_NULL, Option.SUPPRESS_EXCEPTIONS)
+        .build();
+    /*public static String getData(String jsonData, String jsonPath) {
         return JsonPath.parse(jsonData).read(jsonPath);
+    }*/
+
+
+    public static String getData(String jsonData, String jsonPath) {
+        DocumentContext json = using(configuration).parse(jsonData);
+        return json.read(jsonPath);
+    }
+
+    public static String setData(String jsonData, String jsonPath, String newValue){
+        DocumentContext json = using(configuration).parse(jsonData);
+        return json.set(jsonPath, newValue).jsonString();
+    }
+
+    public static String getJsonData(String jsonData, String jsonPath) {
+        DocumentContext json = using(configuration).parse(jsonData);
+        return json.read(jsonPath, JSONObject.class).toJSONString();
+    }
+
+    public static List<String> getJsonPaths(String jsonData, String jsonPath) {
+        Configuration conf = Configuration.builder().options(Option.AS_PATH_LIST, Option.ALWAYS_RETURN_LIST).build();
+        DocumentContext json = using(conf).parse(jsonData);
+        return json.read(jsonPath);
     }
 }
--- jsonpath/src/test/java/com/demo/jasonpath/JsonPathDemoTest.java
+++ jsonpath/src/test/java/com/demo/jasonpath/JsonPathDemoTest.java
@@ -1,9 +1,16 @@
 package com.demo.jasonpath;
 
 
+import net.minidev.json.JSONObject;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
 
 public class JsonPathDemoTest {
 
@@ -39,6 +46,94 @@ public class JsonPathDemoTest {
             "    ]\n" +
             "}";
 
+
+    private String jsonData1 = "{\n" +
+        "    \"tool\": \n" +
+        "    {\n" +
+        "        \"jsonpath\": \n" +
+        "        {\n" +
+        "            \"creator\": \n" +
+        "            {\n" +
+        "                \"name\": \"Jayway Inc.\",\n" +
+        "                \"otp\":\n" +
+        "                {\n" +
+        "                    \"masterKeyId\":\"masterKeyID\",\n" +
+        "                    \"credCypherText\":\"cyphertext\",\n" +
+        "                    \"dekCypherText\":\"dekCyphertext\"\n" +
+        "                },\n" +
+        "                \"location\": \n" +
+        "                [\n" +
+        "                    \"Malmo\",\n" +
+        "                    \"San Francisco\",\n" +
+        "                    \"Helsingborg\"\n" +
+        "                ]\n" +
+        "            },\n" +
+        "            \"password\":\n" +
+        "            {\n" +
+        "                \"masterKeyId\":\"masterKeyID\",\n" +
+        "                \"credCypherText\":\"cyphertext\",\n" +
+        "                \"dekCypherText\":\"dekCyphertext\"\n" +
+        "            }\n" +
+        "        }\n" +
+        "    },\n" +
+        " \n" +
+        "    \"book\": \n" +
+        "    [\n" +
+        "        {\n" +
+        "            \"name\": \"Beginning JSON\",\n" +
+        "            \"price\": 49.99\n" +
+        "        },\n" +
+        " \n" +
+        "        {\n" +
+        "            \"name\": \"JSON Essentials\",\n" +
+        "            \"price\": 9.99\n" +
+        "        },\n" +
+        " \n" +
+        "        {\n" +
+        "            \"name\": \"JSON at Work\",\n" +
+        "            \"price\": 29.99\n" +
+        "        }\n" +
+        "    ]\n" +
+        "}";
+
+    private String jsonData2 = "{\n" +
+        "    \"store\": {\n" +
+        "        \"book\": [\n" +
+        "            {\n" +
+        "                \"category\": \"reference\",\n" +
+        "                \"author\": \"Nigel Rees\",\n" +
+        "                \"title\": \"Sayings of the Century\",\n" +
+        "                \"price\": 8.95\n" +
+        "            },\n" +
+        "            {\n" +
+        "                \"category\": \"fiction\",\n" +
+        "                \"author\": \"Evelyn Waugh\",\n" +
+        "                \"title\": \"Sword of Honour\",\n" +
+        "                \"price\": 12.99\n" +
+        "            },\n" +
+        "            {\n" +
+        "                \"category\": \"fiction\",\n" +
+        "                \"author\": \"Herman Melville\",\n" +
+        "                \"title\": \"Moby Dick\",\n" +
+        "                \"isbn\": \"0-553-21311-3\",\n" +
+        "                \"price\": 8.99\n" +
+        "            },\n" +
+        "            {\n" +
+        "                \"category\": \"fiction\",\n" +
+        "                \"author\": \"J. R. R. Tolkien\",\n" +
+        "                \"title\": \"The Lord of the Rings\",\n" +
+        "                \"isbn\": \"0-395-19395-8\",\n" +
+        "                \"price\": 22.99\n" +
+        "            }\n" +
+        "        ],\n" +
+        "        \"bicycle\": {\n" +
+        "            \"color\": \"red\",\n" +
+        "            \"price\": 19.95\n" +
+        "        }\n" +
+        "    },\n" +
+        "    \"expensive\": 10\n" +
+        "}";
+
     @Test
     public void testcase1(){
         String jsonPath = "$.tool.jsonpath.creator.name";
@@ -49,5 +144,79 @@ public class JsonPathDemoTest {
     public void testcase2(){
         String jsonPath = "$.tool.jsonpath.creator.location[2]";
         assertEquals(JsonPathDemo.getData(jsonData, jsonPath), "Helsingborg");
+    }
+
+    @Test
+    public void testcase3(){
+        String jsonPath = "$.tool.jsonpath.creator.name";
+        String newJsonData = JsonPathDemo.setData(jsonData, jsonPath, "Oracle Corp");
+        assertEquals(JsonPathDemo.getData(newJsonData, jsonPath), "Oracle Corp");
+    }
+
+    @Test
+    public void testcase4(){
+        String jsonPath = "$.tool.jsonpath.password";
+        String data = JsonPathDemo.getJsonData(jsonData1, jsonPath);
+        System.out.println(data);
+    }
+
+    @Test
+    public void getJsonPathsTest(){
+        String jsonPath = "$..author";
+        List<String> expected = Arrays.asList("$['store']['book'][0]['author']",
+            "$['store']['book'][1]['author']",
+            "$['store']['book'][2]['author']",
+            "$['store']['book'][3]['author']");
+
+        List<String> pathList = JsonPathDemo.getJsonPaths(jsonData2, jsonPath);
+        // Ensure List
+        assertThat(pathList, isA(List.class));
+
+        // Ensure Contents
+        assertThat(pathList, is(expected));
+
+        // Ensure Order
+        assertThat(pathList, contains("$['store']['book'][0]['author']",
+            "$['store']['book'][1]['author']",
+            "$['store']['book'][2]['author']",
+            "$['store']['book'][3]['author']"));
+    }
+
+
+    @Test
+    public void getJsonPathsTest2(){
+        String jsonPath = "$..name";
+        List<String> expected = Arrays.asList("$['tool']['jsonpath']['creator']['name']",
+            "$['book'][0]['name']",
+            "$['book'][1]['name']",
+            "$['book'][2]['name']");
+
+        List<String> pathList = JsonPathDemo.getJsonPaths(jsonData1, jsonPath);
+        // Ensure List
+        assertThat(pathList, isA(List.class));
+
+        // Ensure Contents
+        assertThat(pathList, is(expected));
+
+        // Ensure Order
+        assertThat(pathList, contains("$['tool']['jsonpath']['creator']['name']",
+            "$['book'][0]['name']",
+            "$['book'][1]['name']",
+            "$['book'][2]['name']"));
+    }
+
+    @Test
+    public void getJsonPathsTest3(){
+        String jsonPath = "$.tool.jsonpath.password";
+        List<String> expected = Arrays.asList("$['tool']['jsonpath']['password']");
+
+        List<String> pathList = JsonPathDemo.getJsonPaths(jsonData1, jsonPath);
+
+        // Ensure List
+        assertThat(pathList, isA(List.class));
+
+        // Ensure Contents
+        assertThat(pathList, is(expected));
+
     }
 }
